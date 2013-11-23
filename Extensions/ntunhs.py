ac7d02a4a6511589fee31bf4a527942053ccf522
andy fang <andy@mingtak.com.tw>
2013-11-23 05:25:33 +0000

    設計佈景前備份

--- Extensions/ntunhs.py
+++ Extensions/ntunhs.py
@@ -2,6 +2,7 @@
 from plone import api
 
 
+#活動報名表取出活動名稱
 def getTitle(self):
     virtualPath = str(self.REQUEST['VIRTUAL_URL_PARTS'][-1])
     content = api.content.get(path='ntunhs/' + virtualPath)
@@ -14,3 +15,14 @@ def getTitle(self):
     except:
         title = None
     return title
+
+
+#取出頂部導航
+def getTopNav(self):
+    catalog = api.portal.get_tool(name='portal_catalog')
+    items = catalog(portal_type='Folder')
+    result = str()
+    for item in items:
+        if not item.exclude_from_nav:
+            result += "%s\t%s\n" % (item.Title, item.getPath())
+    return result
