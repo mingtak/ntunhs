b6ee59c1a1dd3d4ccc8bc4e8b86b091b310c2f0b
Andy Fang <andy@mingtak.com.tw>
2013-12-20 15:08:24 +0800

    add: browserview, modify:configure.zcml, trashcanview.pt

---
+++ ntunhs.contents/ntunhs/contents/browser/views.py
@@ -0,0 +1,21 @@
+from Products.Five.browser import BrowserView
+from plone import api
+
+
+def removeItem(path):
+    item = api.content.get(path=path)
+    api.content.delete(obj=item)
+
+
+class CleanTrashcan(BrowserView):
+    def __call__(self):
+        if not self.request.has_key('deletecheck'):
+            return
+        removeItems = self.request['deletecheck']
+        if type(removeItems) == type(str()):
+            removeItem(removeItems)
+        else:
+            for item in removeItems:
+                removeItem(item)        
+        trashcan = api.content.get(UID='559f90f72f3042b8ae23278c57e8e08b')
+        return trashcan()
